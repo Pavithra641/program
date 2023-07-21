dc0ace43f9a20ce57f579706ba29dc78aea1c23d
pavithra <muthurajpavithra64@gmail.com>
2023-07-21 13:14:31 +0530

    test

---
+++ src/main.java
@@ -0,0 +1,14 @@
+public class main {
+    public static void main(String[] args){
+        student s1=new student();
+        s1.name="pavithra";
+        s1.dept="ECE";
+
+        student s2=new student();
+        s2.name="petchiyammal";
+        s2.dept="EEE";
+
+        System.out.println(s1.name +" " + s1.dept);
+        System.out.println(s2.name + " " + s2.dept);
+    }
+}
